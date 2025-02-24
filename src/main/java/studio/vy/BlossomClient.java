9f68a09fa0a9601090a464a5556dd6b8fa75d3ec
yunitrish <yunthomas006@gmail.com>
2025-02-24 09:31:28 +0800

    Initial commit

---
+++ src/main/java/studio/vy/BlossomClient.java
@@ -0,0 +1,11 @@
+package studio.vy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlossomClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
