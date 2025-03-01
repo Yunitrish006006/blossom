d0ddf8f3cd3ab00d2cc0676681003f1bc2c59dbf
chai <chainot@hotmail.co.th>
2025-03-01 12:00:57 +0700

    Split Clinet and Server for more clear

--- src/main/java/studio/vy/ModPayload.java
+++ src/main/java/studio/vy/ModPayload.java
@@ -10,8 +10,10 @@ import studio.vy.TeleportSystem.UnitTeleportPayloadS2C;
 public class ModPayload {
     public static void init() {
         PayloadTypeRegistry.playC2S().register(UnitTeleportPayloadC2S.ID, UnitTeleportPayloadC2S.CODEC);
-        PayloadTypeRegistry.playS2C().register(UnitTeleportPayloadS2C.ID, UnitTeleportPayloadS2C.CODEC);
         PayloadTypeRegistry.playC2S().register(ServerSpaceUnitPayloadC2S.ID, ServerSpaceUnitPayloadC2S.CODEC);
+
+
+        PayloadTypeRegistry.playS2C().register(UnitTeleportPayloadS2C.ID, UnitTeleportPayloadS2C.CODEC);
         PayloadTypeRegistry.playS2C().register(SpaceUnitSyncPayloadS2C.ID, SpaceUnitSyncPayloadS2C.CODEC);
 
         ServerPlayNetworking.registerGlobalReceiver(UnitTeleportPayloadC2S.ID, new UnitTeleportPayloadC2S.Receiver());
