7938e9de52522141c2a5140f6f0f0673809f3213
yunitrish <yunthomas006@gmail.com>
2025-04-10 11:31:30 +0800

    Add copper armor recipes to ModRecipeProvider

--- src/main/java/studio/vy/datagen/ModRecipeProvider.java
+++ src/main/java/studio/vy/datagen/ModRecipeProvider.java
@@ -2,6 +2,7 @@ package studio.vy.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.ItemConvertible;
@@ -46,6 +47,39 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                         Items.ROTTEN_FLESH,
                         ModItems.SMOKED_ROTTEN_FLESH,
                         0.35f);
+                offerArmorRecipe(Items.COPPER_INGOT, "armor_copper", ModItems.COPPER_HELMET, ModItems.COPPER_CHESTPLATE, ModItems.COPPER_LEGGINGS, ModItems.COPPER_BOOTS);
+            }
+            public void offerArmorRecipe(ItemConvertible input, String group, ItemConvertible helmet, ItemConvertible chestplate, ItemConvertible leggings, ItemConvertible boots) {
+                this.createShaped(RecipeCategory.COMBAT,helmet)
+                        .input('#', input)
+                        .pattern("###")
+                        .pattern("# #")
+                        .group(group)
+                        .criterion(hasItem(input), this.conditionsFromItem(input))
+                        .offerTo(this.exporter);
+                this.createShaped(RecipeCategory.COMBAT, chestplate)
+                        .input('#', input)
+                        .pattern("# #")
+                        .pattern("###")
+                        .pattern("###")
+                        .group(group)
+                        .criterion(hasItem(input), this.conditionsFromItem(input))
+                        .offerTo(this.exporter);
+                this.createShaped(RecipeCategory.COMBAT, leggings)
+                        .input('#', input)
+                        .pattern("###")
+                        .pattern("# #")
+                        .pattern("# #")
+                        .group(group)
+                        .criterion(hasItem(input), this.conditionsFromItem(input))
+                        .offerTo(this.exporter);
+                this.createShaped(RecipeCategory.COMBAT, boots)
+                        .input('#', input)
+                        .pattern("# #")
+                        .pattern("# #")
+                        .group(group)
+                        .criterion(hasItem(input), this.conditionsFromItem(input))
+                        .offerTo(this.exporter);
             }
         };
     }
